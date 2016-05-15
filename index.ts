8e35548b2c433955ed840d1e81f84a09413249aa
Daniel Martinez <dmartz86@gmail.com>
2016-05-15 10:56:54 -0500

    Fix TypeScript warning

--- index.ts
+++ index.ts
@@ -4,9 +4,9 @@ const log = console.log;
 
 async function blogging() {
   await connect('mongodb://localhost/blog');
-  const daniel = await new Person({name: 'Daniel', role: 'dev'}).save();
-  const intro = await new Post({title: 'This is my new Blog', author: daniel._id}).save();
-  const infor = await new Post({title: 'A funny entry', author: daniel._id}).save();
+  const daniel: any = await new Person({name: 'Daniel', role: 'dev'}).save();
+  const intro: any = await new Post({title: 'This is my new Blog', author: daniel._id}).save();
+  const infor: any = await new Post({title: 'A funny entry', author: daniel._id}).save();
   daniel.posts.push(intro._id);
   daniel.posts.push(infor._id);
   daniel.name = 'MoNoTy';
